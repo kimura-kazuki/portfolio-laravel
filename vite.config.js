3dd2eae4166d7bd433ab4b6e66833e987279a2e0
kazuki <kidsc0me@gmail.com>
2023-11-09 11:25:17 +0900

    Fix: :bug: eslintエラー解消 (#1) @0.1h

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-import { defineConfig, loadEnv } from 'vite';
+// import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import i18n from 'laravel-vue-i18n/vite';
